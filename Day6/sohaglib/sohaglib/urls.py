0c2dfd9dae94c9e77a86f82fe0d7e2579adbb021
josspheenboles <eng.jospheenboles@gmail.com>
2023-02-06 12:51:25 +0200

    Django Day2

---
+++ Day6/sohaglib/sohaglib/urls.py
@@ -0,0 +1,28 @@
+"""sohaglib URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from catagory import views
+from book import views as bookviews
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.List,name='Catagory_List'),
+    path('Book/Add',bookviews.Add,name='Book_Add'),
+    path('Catatory/Add',views.Add,name='Catagory_Add'),
+    path('Catatory/Update/<int:catid>',views.Update,name='Catagory_Update'),
+    path('booklist',bookviews.List,name='Book_List'),
+
+]
